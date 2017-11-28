617794370f99d38651953b3d731540bfceea841d
Ariel <arosalesfarias@gmail.com>
2017-11-27 23:34:08 -0300

    Se calcula TEA correctamente

--- js/domain/CalcTasa.js
+++ js/domain/CalcTasa.js
@@ -7,12 +7,18 @@ class CalcTasa{
         this.tea = 0
     }
     calcularTEA(){
-        this.tea = (this.tasaEfectivaPorPeriodo()*0.01+1)^this.periodosDeCapitalizacion()
+        this.tea = this.decimalAPorcentaje(Math.pow(this.porcentajeADecimal(this.tasaEfectivaPorPeriodo()),this.periodosDeCapitalizacion()))
     }
     tasaEfectivaPorPeriodo(){
-        return (this.tna/DIAS_ANIO)*this.periodo
+        return ((this.tna/DIAS_ANIO)*this.periodo)
     }
     periodosDeCapitalizacion(){
         return DIAS_ANIO/this.periodo
+    }
+    porcentajeADecimal(tasa){
+        return (tasa*0.01)+1 // Ejemplo: 27 a 1.27
+    }
+    decimalAPorcentaje(tasa){
+        return (tasa-1)*100 // Ejemplo : 1.27 a 27
     }
 }
